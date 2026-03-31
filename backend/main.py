edc09587f73f757ab5dc8afdcc66f7ef325063c6
Sadhika Sunil <sadhika.sunilnair@gmail.com>
2026-03-31 22:23:18 +0530

    add railway url to CORS

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI(title="AI Finance Advisor API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins     = ["http://localhost:3000"],
+    allow_origins     = ["https://ai-financial-advisor-production-b3fc.up.railway.app"],
     allow_credentials = True,
     allow_methods     = ["*"],
     allow_headers     = ["*"],
